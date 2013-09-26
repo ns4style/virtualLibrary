84f1755efddf398d41585baeb0f4a3880a893325
ns4style <ns4style@mail.ru>
2013-09-26 19:00:37 +0800

    добавлен класс сервлета

---
+++ src/servlet/Library_servlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Library_servlet")
+public class Library_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Library_servlet() {
+		super();
+	}
+
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.service(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
