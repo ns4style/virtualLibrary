385a9c83e0de827f4a8c62b8b01c579ce6446aad
Haderach <muad.dib.kwisatz@gmail.com>
2013-09-27 03:57:02 -0400

    Добавлен шаблон под парс респонса, map_handlers содержит обработчики

--- src/servlet/map_handlers.java
+++ src/servlet/map_handlers.java
@@ -1,5 +1,13 @@
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 /*
  * Реализация обработчиков запросов, имена методов соответствуют
@@ -8,23 +16,48 @@ package servlet;
 
 public class map_handlers {
 	
-	public static void admin() {
-		
+	public static void admin(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.println("<H1>Admin page</H1>");
+		} catch (IOException ex) {
+			System.err.println("Error in admin page! " + ex.getMessage());
+		}
 	}
 	
-	public static void index() {
-		
+	public static void index(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.println("<H1>Index page</H1>");
+		} catch (IOException ex) {
+			System.err.println("Error in admin page! " + ex.getMessage());
+		}
 	}
 	
-	public static void books() {
-		
+	public static void books(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.println("<H1>Books page</H1>");
+		} catch (IOException ex) {
+			System.err.println("Error in admin page! " + ex.getMessage());
+		}
 	}
 	
-	public static void register() {
-		
+	public static void register(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.println("<H1>Register page</H1>");
+		} catch (IOException ex) {
+			System.err.println("Error in admin page! " + ex.getMessage());
+		}
 	}
 	
-	public static void cabinet() {
-		
+	public static void cabinet(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			PrintWriter pw = response.getWriter();
+			pw.println("<H1>Cabinet page</H1>");
+		} catch (IOException ex) {
+			System.err.println("Error in admin page! " + ex.getMessage());
+		}
 	}
 }
