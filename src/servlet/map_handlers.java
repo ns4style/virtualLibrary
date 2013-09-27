174ab484011581c856263fe0b3fb1e39d15606d3
g33k <g33k@g33k0n6eb1an>
2013-09-27 03:55:22 -0400

    Добавлен шаблон под парс респонса, map_handlers содержит обработчики

---
+++ src/servlet/map_handlers.java
@@ -0,0 +1,30 @@
+package servlet;
+
+
+/*
+ * Реализация обработчиков запросов, имена методов соответствуют
+ * запрашиваемой странице
+ */
+
+public class map_handlers {
+	
+	public static void admin() {
+		
+	}
+	
+	public static void index() {
+		
+	}
+	
+	public static void books() {
+		
+	}
+	
+	public static void register() {
+		
+	}
+	
+	public static void cabinet() {
+		
+	}
+}
