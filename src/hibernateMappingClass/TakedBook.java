ea111b4d518ec8bf7c88332ae65f6b6d79c04d9f
blitz156 <xxxdima92@gmail.com>
2013-09-30 19:29:37 +0800

    add entite some ORM

--- src/hibernateMappingClass/TakedBook.java
+++ src/hibernateMappingClass/TakedBook.java
@@ -3,11 +3,15 @@ package hibernateMappingClass;
 import java.sql.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
+@Entity
+@Table(name = "taked_books")
 public class TakedBook {
 	@Id
 	@GeneratedValue(generator = "increment")
