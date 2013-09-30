c9997336b4e06f2b0bef2f18386144fdd37c91c0
blitz156 <xxxdima92@gmail.com>
2013-09-30 17:18:04 +0800

    refactor mainclass

--- src/servlet/LibraryServlet.java
+++ src/servlet/LibraryServlet.java
@@ -12,9 +12,6 @@ public class LibraryServlet extends HttpServlet {
 
 	public LibraryServlet() {
 		super();
-		
-		
-		
 	}
 
 	public void service(HttpServletRequest request, HttpServletResponse response)
