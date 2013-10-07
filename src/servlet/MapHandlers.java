2ba6c6c22424f1d4b9418169fd3e18aa1dcd3283
blitz156 <xxxdima92@gmail.com>
2013-10-07 17:07:14 +0800

    hui2

--- src/servlet/MapHandlers.java
+++ src/servlet/MapHandlers.java
@@ -6,7 +6,7 @@ import hibernateMappingClass.Book;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -32,20 +32,22 @@ public class MapHandlers {
 	public static void index(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		IndexClass.init(request, response);
-		Map parameters=request.getParameterMap();
-		if (parameters.containsKey("action"))
-			if (request.getParameter("action").equals("reg")){
+		Map parameters = request.getParameterMap();
+		if (parameters.containsKey("action")) {
+			if (request.getParameter("action").equals("reg")) {
 				IndexClass.Registration(request, response);
 				return;
 			}
-		if (parameters.containsKey("action"))
-			if (request.getParameter("action").equals("prereg")){
+		}
+		if (parameters.containsKey("action")) {
+			if (request.getParameter("action").equals("prereg")) {
 				IndexClass.preRegistration(request, response);
 				return;
-				}
+			}
+		}
+		RequestDispatcher rd = request
+				.getRequestDispatcher("/templates/index.jsp");
 		rd.forward(request, response);
-		RequestDispatcher rd = request.getRequestDispatcher("/templates/index.jsp");
-		rd.forward(request, response);	
 	}
 
 	public static void books(HttpServletRequest request,
@@ -60,32 +62,13 @@ public class MapHandlers {
 			e.printStackTrace();
 		}
 
-		// получение двумерного массива, где
-		// [i][0] - имя книги
-		// [i][j] - пути картинок для книг
-		String books_pathArray[][] = new String[books.size()][];
-
-		for (int i = 0; i < books.size(); i++) {
-			books_pathArray[i] = new String[books.get(i).getImages().size() + 1];
-			books_pathArray[i][0] = books.get(i).getName();
-
-			Iterator it = books.get(i).getImages().iterator();
-			int j = 1;
-			while (it.hasNext()) {
-				Image image = (Image) it.next();
-				books_pathArray[i][j] = image.getPath();
-				j++;
-			}
-		}
-		
-		//request.setAttribute("books_pathArray", books_pathArray);
 		request.setAttribute("books", books);
 
 		RequestDispatcher rd = request
 				.getRequestDispatcher("/templates/books.jsp");
 		rd.forward(request, response);
 	}
-		
+
 	public static void register(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
