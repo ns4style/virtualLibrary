2ff021275f541721ed44bd9b7a6b6942aadddcfd
blitz156 <xxxdima92@gmail.com>
2013-10-21 18:31:53 +0800

    merge nn

--- src/hibernateMappingClass/User.java
+++ src/hibernateMappingClass/User.java
@@ -1,15 +1,9 @@
 package hibernateMappingClass;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
